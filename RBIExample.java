f221955c77fbcefbe865eb2586697f8fccc33a7c
ak877157 <61462914+ak877157@users.noreply.github.com>
2022-02-14 15:14:36 +0530

    Update RBIExample.java

--- RBIExample.java
+++ RBIExample.java
@@ -1,5 +1,26 @@
 package com.org.gen.coding.test;
 
+class Customer{
+	
+}
+
+class Account{
+	
+}
+
+class RBI{
+	
+}
+
+class SBI extends RBI{
+	
+}
+
+class ICICI extends RBI {
+	
+}
+
+
 public class RBIExample {
 
 	public static void main(String[] args) {
